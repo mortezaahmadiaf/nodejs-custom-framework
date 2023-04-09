152bcf0842fc78b17ef9635a7361891daf09d392
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-09 19:08:53 +0430

    add api ti generate and decorate jwt

--- src/Controller/v1/test-controller.ts
+++ src/Controller/v1/test-controller.ts
@@ -1,4 +1,5 @@
 import { Response, Request, NextFunction } from "express";
+import { jwtDecorator, jwtGenerator } from "../../Feature/Utilities";
 export class TestController {
   add(req: Request, response: Response, next: NextFunction) {
     response.send("test post ");
@@ -16,6 +17,12 @@ export class TestController {
     response.send("test getAll ");
   }
   patch(req: Request, response: Response, next: NextFunction) {
+    response.send("test patch ");
+  }
+  generateJWT(req: Request, response: Response, next: NextFunction) {
+    response.send("test patch ");
+  }
+  decorateJWT(req: Request, response: Response, next: NextFunction) {
     response.send("test patch ");
   }
 }
--- src/Router/v1/test-route.ts
+++ src/Router/v1/test-route.ts
@@ -1,5 +1,6 @@
 import { BaseRouter } from "../../Feature/Utilities";
 import { TestController } from "../../Controller/v1";
+import { NextFunction, Response, Request } from "express";
 class TestRoutes extends BaseRouter {
   constructor() {
     super(TestController);
@@ -8,7 +9,25 @@ class TestRoutes extends BaseRouter {
 
   init() {
     super.init();
+
+    this.router
+      .route("jwt-generate")
+      .post((req: Request, res: Response, next: NextFunction) => {
+        this.generateJWT(req, res, next);
+      });
+    this.router
+      .route("jwt-decorate")
+      .post((req: Request, res: Response, next: NextFunction) => {
+        this.decorateJWT(req, res, next);
+      });
   }
+
+  private generateJWT = (req: Request, res: Response, next: NextFunction) => {
+    this.controller.generateJWT(req, res, next);
+  };
+  private decorateJWT = (req: Request, res: Response, next: NextFunction) => {
+    this.controller.decorateJWT(req, res, next);
+  };
 }
 
 export const TestRouter = new TestRoutes().router;
