3b2fe1e94ed09343821db571a6bdd4c8f490aae4
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-12 12:55:01 +0430

    add models to profle

--- src/Models/Profile-Model/profile-manager.ts
+++ src/Models/Profile-Model/profile-manager.ts
@@ -1,8 +1,23 @@
-class ProfileManager {
-  create = async () => {};
-  update = async () => {};
-  delete = async () => {};
-  getById = async () => {};
-  getAll = async () => {};
-  patch = async () => {};
+import { ProfileModel } from "./profile-model";
+import { IProfile } from "../schemas";
+export class ProfileManager {
+  private profileModel: ProfileModel = new ProfileModel();
+  create = (props: IProfile) => {
+    return this.profileModel.create(props);
+  };
+  update = (props: IProfile) => {
+    return this.profileModel.update(props);
+  };
+  delete = (id: string) => {
+    return this.profileModel.delete(id);
+  };
+  getById = (id: string) => {
+    return this.profileModel.getById();
+  };
+  getAll = () => {
+    return this.profileModel.getAll();
+  };
+  patch = () => {
+    return this.profileModel.patch();
+  };
 }
