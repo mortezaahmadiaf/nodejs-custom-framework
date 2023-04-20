e40861fcedadc6756de682a4a69b0e05664b69b8
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-20 14:09:11 +0430

    change path

--- src/Features/Validations/v1/user.ts
+++ src/Features/Validations/v1/user.ts
@@ -2,7 +2,6 @@ import { IsUUID, Length, IsDefined } from "class-validator";
 import { IUser, IUpdateUser } from "../../../Models/schemas";
 export class UserValidator implements IUser {
   @IsDefined()
-  @Length(10, 20)
   username!: string;
   @IsDefined()
   @Length(4, 20)
--- src/Routers/v1/user-routes.ts
+++ src/Routers/v1/user-routes.ts
@@ -1,5 +1,12 @@
 import { BaseRoutes } from "../../Features/Utilities";
 import { UserController } from "../../Controllers";
+import { authJwt, validationMw } from "app/Features/Middlewares";
+import {
+  UserUpdateValidator,
+  UserValidator,
+  IdValidator,
+} from "app/Features/Validations";
+import { AccessPolicy, ValidationPolicy } from "app/Features/Policies";
 class UserRoute extends BaseRoutes {
   constructor() {
     super(UserController);
@@ -7,7 +14,18 @@ class UserRoute extends BaseRoutes {
   }
 
   init() {
-    super.init();
+    const accessPolicy: AccessPolicy = new AccessPolicy();
+    accessPolicy.post = authJwt;
+    accessPolicy.put = authJwt;
+    accessPolicy.get = authJwt;
+    accessPolicy.getById = authJwt;
+    accessPolicy.delete = authJwt;
+    accessPolicy.patch = authJwt;
+    const validationPolicy: ValidationPolicy = new ValidationPolicy();
+    validationPolicy.post = validationMw(UserValidator);
+    validationPolicy.put = validationMw(UserUpdateValidator);
+    validationPolicy.delete = validationMw(IdValidator);
+    super.init(accessPolicy, validationPolicy);
   }
 }
 
