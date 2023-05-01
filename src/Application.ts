8ce10923a729d5739083eb07451dc6f44f2ef27b
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-05-01 20:58:48 +0430

    test rabit mq

--- src/Application.ts
+++ src/Application.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import { Mysql } from "./Features/DB-Connections";
+import amqplib from "amqplib/callback_api";
+import { main } from "./rabit";
 
 dotenv.config();
 export class Application {
@@ -16,6 +18,10 @@ export class Application {
     this.database_connection();
     this.mioddelware();
     this.router();
+    this.rabitMQ();
+    // main().catch((error) => console.error(error));
+
+    // this.rabitMQ();
   }
   private router = () => {
     this.app.use("/test", TestRouter);
@@ -30,6 +36,38 @@ export class Application {
     } catch (error) {
       console.error("mysql : Unable to connect to the database:", error);
     }
+  };
+  private rabitMQ = () => {
+    const queue = "tasks";
+    amqplib.connect("amqp://localhost:5672", (err, conn) => {
+      if (err) throw err;
+      // Listener
+      conn.createChannel((err, ch2) => {
+        if (err) throw err;
+
+        ch2.assertQueue(queue);
+
+        ch2.consume(queue, (msg) => {
+          if (msg !== null) {
+            console.log(msg.content.toString(), "Receive");
+            ch2.ack(msg);
+          } else {
+            console.log("Consumer cancelled by server");
+          }
+        });
+      });
+      // Sender
+      conn.createChannel((err, ch1) => {
+        if (err) throw err;
+
+        ch1.assertQueue(queue);
+
+        setInterval(() => {
+          ch1.sendToQueue(queue, Buffer.from("message"));
+          console.log("send Message");
+        }, 1000);
+      });
+    });
   };
 
   private mioddelware = () => {
