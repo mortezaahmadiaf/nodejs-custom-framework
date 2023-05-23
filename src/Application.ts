071e12184941f5a5af4df6b317b3c60f17752a75
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-05-23 20:07:39 +0430

    add router upload add static directory

--- src/Application.ts
+++ src/Application.ts
@@ -1,6 +1,11 @@
 import express, { Express } from "express";
-import { TestRouter, UserRouter, ProfileRouter } from "./Routers/v1";
-import { logger } from "./Features/Middlewares";
+import {
+  TestRouter,
+  UserRouter,
+  ProfileRouter,
+  FileRouter,
+} from "./Routers/v1";
+import { logger, authJwt } from "./Features/Middlewares";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -19,27 +24,29 @@ export class Application {
     this.mioddelware();
     this.router();
     this.rabbitMQlistening();
+    this.static();
   }
   private router = () => {
     this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(swagger));
     this.app.use("/test", TestRouter);
+    this.app.use("/upload", FileRouter);
     this.app.use("/user", UserRouter);
     this.app.use("/profile", ProfileRouter);
   };
 
   private database_connection = async () => {
-    // try {
-    //   await Mysql.authenticate();
-    //   console.log("mysql : Connection has been established successfully.");
-    // } catch (error) {
-    //   console.error("mysql : Unable to connect to the database:", error);
-    // }
+    try {
+      await Mysql.authenticate();
+      console.log("mysql : Connection has been established successfully.");
+    } catch (error) {
+      console.error("mysql : Unable to connect to the database:", error);
+    }
   };
   async rabbitMQlistening() {
-    // try {
-    //   const rabbit = new RabbitMQConsume();
-    //   await rabbit.consumeAll();
-    // } catch (error) {}
+    try {
+      const rabbit = new RabbitMQConsume();
+      await rabbit.consumeAll();
+    } catch (error) {}
   }
   private mioddelware = () => {
     this.app.use(cors());
@@ -47,6 +54,11 @@ export class Application {
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(logger);
   };
+  private static(): void {
+    // this.app.use(authJwt);
+    // add jwt authentication
+    this.app.use(express.static("./files"));
+  }
   runServer = () => {
     this.app.listen(this.Port, () => {
       console.log(`express server start on port ${this.Port}`);
